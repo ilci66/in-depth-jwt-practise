c6caf8c0e7c493ddaece5fbbd4b263a1f771a087
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-08-05 12:11:59 +0200

    working on register route

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 const User = mongoose.model('User');
 const passport = require('passport');
 const utils = require('../lib/utils');
+//for most of the hashing and stuff I would normally use bcrypt module but learning 
+//something else is always good
+
 
 // TODO
 router.get('/protected', (req, res, next) => {
@@ -11,7 +14,28 @@ router.get('/protected', (req, res, next) => {
 // TODO
 router.post('/login', function(req, res, next){});
 
-// TODO
-router.post('/register', function(req, res, next){});
+router.post('/register', function(req, res, next){
+  const saltHash = utils.genPassword(req.body.password);
+  
+  const salt = saltHash.salt;
+  const hash = saltHash.hash;
+
+  const newUser = new User({
+      username: req.body.username,
+      hash: hash,
+      salt: salt
+  });
+  try {
+    newUser.save()
+      .then((user) => {
+        //returning a success message as well as the user, more useful than just returning the user
+        //the jwt will be issued here, jwt will be created in the utils
+        res.json({ success: true, user: user });
+      });
+  } catch (err) {
+    res.json({ success: false, msg: err });
+  }
+
+});
 
 module.exports = router;
