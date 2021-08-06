7caea44f93c618b182d9467083d56b573b1b8421
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-08-06 12:27:26 +0200

    added notes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-
+* Include your own database connection string in the .env file to use the app. Mostly using the backend configs, modified some parts of it and added notes while doing for future reference.
+* As I'm not yet familiar with angular in /angular/src/app/services/auth.service.ts only taking notes of the parts I will be using in my future projects.
 * Written tutorial - https://www.zachgollwitzer.com/posts/2020/passport-js/
 * Video tutorial - https://www.youtube.com/playlist?list=PLYQSCk-qyTW2ewJ05f_GKHtTIzjynDgjK
--- angular/src/app/services/auth.service.ts
+++ angular/src/app/services/auth.service.ts
@@ -1,18 +1,47 @@
 import { Injectable } from '@angular/core';
 import * as moment from "moment";
+//gotta look for an alternative to momentjs in future projects but works for this project
+
 
 @Injectable()
 export class AuthService {
 
     constructor() {}
           
-    setLocalStorage(responseObj) {}          
+    setLocalStorage(responseObj) {
 
-    logout() {}
+        // Adds the expiration time defined on the JWT to the current moment
+        const expiresAt = moment().add(Number.parseInt(responseObj.expiresIn), 'days');
 
-    isLoggedIn() {}
+        localStorage.setItem('id_token', responseObj.token);
+        localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
+    }          
 
-    isLoggedOut() {}
+    logout() {
+        //removing it from the local storage is to logout and maybe login with another username 
+        localStorage.removeItem("id_token");
+        localStorage.removeItem("expires_at");
+    }
 
-    getExpiration() {}    
+    public isLoggedIn() {
+        //Is a "second" before? "year", "month" etc. could also be used instead
+        //In a javascript class so "this" is used, if the moment() is returned 
+        //isLoggendin will return false, usage of the second parameter is important
+        return moment().isBefore(this.getExpiration(), "second");
+    }
+
+    isLoggedOut() {
+        return !this.isLoggedIn();
+    }
+
+    getExpiration() {
+        const expiration = localStorage.getItem("expires_at");
+        if (expiration) {
+            const expiresAt = JSON.parse(expiration);
+            return moment(expiresAt);
+        } else {
+            //gets "now"
+            return moment();
+        }
+    }    
 }
